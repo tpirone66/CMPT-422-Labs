77d009578d733b3751ee8f1d69319ec961afd635
tpirone66 <trevor.pirone1@marist.edu>
2018-04-04 10:19:49 -0400

    Pirone Lab 4 File
    Hourglass feature where once the LEDs finish lighting up, a song plays. When the song is complete, the Servo motor rotates.

---
+++ Pirone_Lab4.ino
@@ -0,0 +1,269 @@
+#include <Servo.h>
+Servo myServo;
+const int switchPin = 9;
+unsigned long previousTime = 0; 
+int switchState = 0; 
+int prevSwitchState = 0; 
+int led = 4;
+long interval = 2000;
+int pos = 0;
+int ledOn = false;
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+#define melodyPin 8
+int randNum = random(4, 8); 
+//the melody
+int melody[] = {
+  NOTE_E7, NOTE_E7, 0, NOTE_E7,
+  0, NOTE_C7, NOTE_E7, 0,
+  NOTE_G7, 0, 0,  0,
+  NOTE_G6, 0, 0, 0,
+ 
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+ 
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0,
+ 
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+ 
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0
+};
+//tempo of the song
+int tempo[] = {
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+};
+void setup() {
+  for (int x = 4; x < 8; x++) {
+    pinMode(x, OUTPUT);
+  }
+  pinMode(switchPin, INPUT);
+  pinMode(8, OUTPUT);
+  myServo.attach(10);
+}
+
+void loop() {
+  unsigned long currentTime = millis();
+
+  if (currentTime - previousTime > interval) {
+    previousTime = currentTime;
+    if (led < 7) {
+      digitalWrite(led, HIGH);
+    }
+    led++;
+
+    if (led == 7) {
+      digitalWrite(6, HIGH);
+      delay(2000);
+      digitalWrite(7, HIGH);
+      delay(2000);
+      int ledOn = true;
+      if (ledOn == true) {
+        digitalWrite(4, LOW);
+        digitalWrite(5, LOW);
+        digitalWrite(6, LOW);
+        digitalWrite(7, LOW);
+        delay(2000);
+        ledOn = false;
+      }
+        if (ledOn == false) {
+          playSong(0);
+          moveServo();
+        }
+    }
+  }
+  
+  switchState = digitalRead(switchPin);
+
+  if (switchState != prevSwitchState) {
+    for (int x = 4; x < 8; x++) {
+      digitalWrite(x, LOW);
+    }
+    
+    led = 4;
+
+    previousTime = currentTime;
+  }
+  
+  prevSwitchState = switchState;
+}
+
+int song = 0;
+
+//play the song
+void playSong(int s) {
+  song = s;
+  if (s == 0) {
+    Serial.println(" 'Mario Theme'");
+    int size = sizeof(melody) / sizeof(int);
+    for (int thisNote = 0; thisNote < size; thisNote++) {
+      int noteDuration = 1000 / tempo[thisNote];
+      buzz(melodyPin, melody[thisNote], noteDuration);
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+      buzz(melodyPin, 0, noteDuration);
+    }
+  }
+}
+
+//function that plays the sound
+void buzz(int targetPin, long frequency, long length) {
+  unsigned int pin = (rand() % 4 + 4);
+  digitalWrite(7, LOW);
+//  digitalWrite(6, HIGH);
+//  digitalWrite(5, HIGH);
+  digitalWrite(6, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(pin, HIGH);
+  long delayValue = 1000000 / frequency / 2; 
+  long numCycles = frequency * length / 1000; 
+  for (long i = 0; i < numCycles; i++) { 
+    digitalWrite(targetPin, HIGH); 
+    delayMicroseconds(delayValue); 
+    digitalWrite(targetPin, LOW); 
+    delayMicroseconds(delayValue); 
+  }
+//  digitalWrite(7, HIGH);
+//  digitalWrite(6, LOW);
+//  digitalWrite(5, LOW);
+//  digitalWrite(4, HIGH);
+}
+
+//function that moves the servo
+void moveServo() {
+  digitalWrite(7, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(4, LOW);
+  delay(1000);
+  for (pos = 0; pos <= 180; pos += 1) { 
+    myServo.write(pos);              
+    delay(15);                       
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { 
+    myServo.write(pos);              
+    delay(15);                       
+  }
+}
+
